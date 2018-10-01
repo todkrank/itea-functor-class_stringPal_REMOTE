0db85e380dcae24cf94086e5431f697f1d243034
todkrank <ivandavyd4uk@gmail.com>
2018-10-01 14:27:11 +0300

    empty library project

---
+++ iteafunctorclass_stringpal.cpp
@@ -0,0 +1,6 @@
+#include "iteafunctorclass_stringpal.h"
+
+
+Iteafunctorclass_stringPal::Iteafunctorclass_stringPal()
+{
+}
---
+++ iteafunctorclass_stringpal.h
@@ -0,0 +1,13 @@
+#ifndef ITEAFUNCTORCLASS_STRINGPAL_H
+#define ITEAFUNCTORCLASS_STRINGPAL_H
+
+#include "itea-functor-class_stringpal_global.h"
+
+class ITEAFUNCTORCLASS_STRINGPALSHARED_EXPORT Iteafunctorclass_stringPal
+{
+
+public:
+    Iteafunctorclass_stringPal();
+};
+
+#endif // ITEAFUNCTORCLASS_STRINGPAL_H
